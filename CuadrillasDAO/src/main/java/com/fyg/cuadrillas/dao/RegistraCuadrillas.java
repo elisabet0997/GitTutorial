e17ccac0d0dca65d4485591bafb232b7ae0099a6
elisabet0997 <elisabet.vasquez@fygsolutions.com>
2016-09-01 16:13:02 -0500

    subida d pruebA
    prueba

---
+++ CuadrillasDAO/src/main/java/com/fyg/cuadrillas/dao/RegistraCuadrillas.java
@@ -0,0 +1,8 @@
+package com.fyg.cuadrillas.dao;
+
+public class RegistraCuadrillas {
+//aqui van los metodos que hacen las consultas a la base de datos (INSETS)
+	//Las operaciones (consultas) que se van a realizar se van a dividir en dos partes 
+	//uno que haga las consultas(sólo selects) y en otro las operciones(insertar,
+	//modificar, eliminar) (una cclase para registrar y otra para consultar) 
+}
